878c6d581fc8e034593d5e4eb045d1c2328988c1
carson mulligan <carsonmulligan45@gmail.com>
2022-05-16 13:52:50 +0100

    Controller, view, and router working. Now to make it pretty.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # config/routes.rb
-  get 'ask', to: 'pages#ask'
+  get 'ask', to: 'questions#ask'
   # Generic syntax:
   # verb 'path', to: 'controller#action'
+  get 'answer', to: 'questions#answer'
 end
